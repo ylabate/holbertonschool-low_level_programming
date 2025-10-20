45a7aad76febd64cdbf764d720b77b6a55735596
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-20 14:35:51 +0200

    feat: add a script that says positif or negatf randomly

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n == 0)
+	{
+		printf("is zero")
+	}
+	if (n > 0)
+	{
+		printf("is negative")
+	}
+	if (n < 0)
+	{
+		printf("is positive")
+	}
+	return (0);
+}
---
+++ variables_if_else_while/README.md
@@ -0,0 +1,4 @@
+# variable, if, else, while...
+
+### 0-positive_or_negative.c
+gen a random number and say if it's negatif or positif or zero
