ea3e7de7a8ead1f6b7f166c1f71d2d0f68e18290
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-22 16:30:12 +0200

    fix: reversed > and <

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
-*int _islower - that say if the entry is lowercase
+* _islower - that say if the entry is lowercase
+* @e: what do you want more
 *
 *Return: Always 0.
 *
 */
-int r;
-char e;
 
 int _islower(char e)
 {
-	if (e >= 'a' && e <= 'z')
+	int r;
+
+	if (e <= 'a' && e >= 'z')
 		r = 0;
 	else
 		r = 1;
-return(r);
+return (r);
 }
