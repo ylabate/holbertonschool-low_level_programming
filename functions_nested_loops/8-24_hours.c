f9f1b2ecd6b5c262164c5d813767b5b2a36ff672
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-23 16:06:25 +0200

    chore: more readable

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@ void jack_bauer(void)
 	int a;
 	int b;
 
-	for(a = 0;a < 24;a++)
+	for (a = 0; a < 24; a++)
 	{
-		for(b = 0;b < 60;b++)
+		for (b = 0; b < 60; b++)
 		{
 			_putchar('0' + (a / 10));
 			_putchar('0' + (a % 10));
