b2acb4c5abbb18ebea4b039f065e7f778c8abb76
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-24 09:56:52 +0200

    fix: fix a syntax error

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
 * add - add the two entry together
+* @a: sasasasa
+* @b: sosoososs
 * Return: return the result
+*
 */
 
 int add(int a, int b)
 {
 	int c;
-+ 
-	c = (a + b)
 
-return(c);
+	c = a + b;
+return (c);
 }
