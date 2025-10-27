a73011a3fcad773b93e95bbbf91d72118bfabdae
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-27 09:57:23 +0000

    fix: update function documentation for multiplication parameters

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
 * _isdigit - check if the entry is an uppercase
-* @c: entry point
+* @a: the first integer for multiplication
+* @b: the second integer for multiplication
 * Return: return the result
 */
 
 int mul(int a, int b)
 {
 	int c;
+
 	c = (a * b);
 return (c);
 }
