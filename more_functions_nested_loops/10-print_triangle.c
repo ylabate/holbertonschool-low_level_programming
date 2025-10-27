0f10cda13a312b86fef6678e73e592f72dcdedaf
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-27 14:04:23 +0000

    feat: add print_triangle function to print a triangle of specified size

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line.
+ * @size: the size of the square
+ */
+
+ void print_triangle(int n)
+ {
+	int a;
+	int b;
+
+	for (a = n; a > 0; a--)
+	{
+		for (b = 0; b < n; b++)
+		{
+			if (b < (a - 1))
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+ }
