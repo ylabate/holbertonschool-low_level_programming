b765a9cbcc7856c605f7ea792ed8ee9653dfe8c4
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-27 13:33:24 +0000

    feat: implement FizzBuzz program to print numbers from 1 to 100 with Fizz, Buzz, and FizzBuzz conditions

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from 1 to 100 with Fizz, Buzz, and FizzBuzz conditions
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a != 101; a++)
+	{
+		if ((a % 3) == 0 && (a % 5) == 0)
+			printf("FizzBuzz");
+		else if ((a % 3) == 0)
+			printf("Fizz");
+		else if ((a % 5) == 0)
+			printf("Buzz");
+		else
+			printf("%d", a);
+		if (a != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+return (0);
+}
