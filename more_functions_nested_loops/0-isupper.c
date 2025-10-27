52d680b6b827f7b0f5031e7a2da9f308b0a6df22
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-27 09:25:53 +0100

    feat: add a is upper function

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* times_table - show the all multiplication tables
+* Return: return the result
+*/
+
+int _isupper(int c)
+{
+	int a;
+	if (c >= 65 && c <= 89)
+		a = 1;
+	else
+		a = 0;
+return (a);
+}
