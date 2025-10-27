098ebf99bddfdb6972df47ab8b2a88ad55ef621f
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-27 09:42:18 +0000

    feat: implement multiplication function

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _isdigit - check if the entry is an uppercase
+* @c: entry point
+* Return: return the result
+*/
+
+int mul(int a, int b)
+{
+	int c;
+	c = (a + b);
+return (c);
+}
