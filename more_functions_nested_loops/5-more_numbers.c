7c71334175fa4c263cbd6b8b1820a38fb9e5ed8c
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-27 11:00:52 +0000

    feat: implement more_numbers function to print numbers 0-14, 10 times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+* print_most_numbers - print all number from 0 to 9 except 2 and 4
+*/
+
+void more_numbers(void)
+{
+	int a;
+	int b;
+	int c;
+	int i;
+	int i_f;
+	int i_l;
+
+	for (c = 0; c < 11; c++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 9)
+			{
+				i_f = (i / 10);
+				i_l = (i % 10);
+				for (a = 0; a < 2; a++)
+				{
+					if (a == 0)
+					{
+						b = i_f;
+					}
+					else
+					{
+						b = i_l;
+					}
+					_putchar(b + '0');
+				}
+			}
+			else
+				_putchar(i + '0');
+		}
+		_putchar('\n');
+	}
+}
