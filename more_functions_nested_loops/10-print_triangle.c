8a0380f980eed9390f835693877642d8a74c4a4e
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-27 14:06:00 +0000

    fix: correct logic in print_triangle function to handle non-positive sizes

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,15 +10,18 @@
 	int a;
 	int b;
 
-	for (a = n; a > 0; a--)
-	{
-		for (b = 0; b < n; b++)
+	if (!(n <= 0))
+		for (a = n; a > 0; a--)
 		{
-			if (b < (a - 1))
-				_putchar(' ');
-			else
-				_putchar('#');
+			for (b = 0; b < n; b++)
+			{
+				if (b < (a - 1))
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
-	}
+	else
+		_putchar('\n')
  }
