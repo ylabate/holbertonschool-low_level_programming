108cdd58bd161f5b8a02d0520f59b67ad44aca3e
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-28 10:10:13 +0000

    feat: add _strlen function to calculate string length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the two entry integer
+ * @a: the first number to get swapped
+ * @b: the second number to get swapped
+ */
+
+ int _strlen(char *s)
+{
+	int l;
+
+	for (l = 0; s[l] != '\0'; l++)
+		;
+return (l);
+}
