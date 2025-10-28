b6c0f4048629e603531651afae7108d86b6a7cc0
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-28 08:20:51 +0000

    fix: correct logic and variable types in prime factor program

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 /**
  * main - print the largest prime number of 1231952
@@ -7,14 +8,22 @@
 
 int main(void)
 {
-	int o = 1231952;
+	long int o = 612852475143;
 	int i;
+	int a;
 
-	for (i = o; i != 0; i--)
-		if ((o % i) == 0)
+	for (i = (o - 1); i != 1; i--)
+		if ((i % o) == 0)
 		{
-			printf("%i\n", i);
-			return (0);
+			for (a = (i - 1); a != 0; a--)
+			{
+				if (a == 0)
+				{
+					printf("%i", i);
+					return (0);
+				}
+
+			}
 		}
 return (1);
 }
