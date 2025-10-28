562aa774756d072c828e33eac87cb6cbdaf12aac
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-28 14:51:20 +0000

    feat: implement puts_half function to print half of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: the string to print
+ *
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int n;
+
+	for (n = 0; str[n] != '\0'; n++)
+	;
+	if ((n % 2) != 0)
+	{
+		n++;
+	}
+
+	for (i = 0; i != n; i++)
+	{
+		if (i >= ((n / 2) - 1))
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
