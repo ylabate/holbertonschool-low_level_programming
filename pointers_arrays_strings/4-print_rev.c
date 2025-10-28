13278b8a43018b2b7e53e7351e74fac79958d54e
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-28 10:27:03 +0000

    feat: add print_rev function to print a string in reverse followed by a new line

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string reversed followed by a new line
+ * @s: the string to print
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int a;
+	int max;
+
+	for (max = 0; s[max] != '\0'; max++)
+		;
+	for (a = max; a != 0; a--)
+		_putchar(s[a]);
+	_putchar('\n');
+}
