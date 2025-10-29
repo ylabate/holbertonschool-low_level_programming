7bec55cb61e839bc7982369b4697c2ddfc60152f
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-29 08:17:30 +0000

    fix: add condition to check for non-negative array size in print_array function

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i != n; i++)
-	{
-		printf("%i", a[i]);
-		if (i != (n - 1))
-			printf(", ");
-	}
+	if (n >= 0)
+		for (i = 0; i != n; i++)
+		{
+			printf("%i", a[i]);
+			if (i != (n - 1))
+				printf(", ");
+		}
 printf("\n");
 }
