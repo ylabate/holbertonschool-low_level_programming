5001614a9197d8e625beaacdc2ae53abf2c66079
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-29 09:17:26 +0000

    feat: add _strcat function to concatenate two strings

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - copies the string pointed to by src to dest
+ * @dest: the destination buffer
+ * @src: the source string
+ *
+ * Return: pointer to dest
+ */
+
+ char *_strcat(char *dest, char *src)
+{
+	int ndest;
+	int nsrc;
+	int i;
+
+	for (ndest = 0; dest[ndest] != '\0'; ndest++)
+	;
+	for (nsrc = 0; src[nsrc] != '\0'; nsrc++)
+	;
+	for (i = 0; i != (nsrc + ndest); i++)
+	{
+		dest[(ndest + i)] = src[i];
+	}
+return (dest);
+}
