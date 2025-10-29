50647793a0ad298b07e5b35a135867fd10baddb5
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-29 13:28:05 +0000

    feat: implement _strncat function to replace n first char from a string to anither

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strncat - concatenates the string pointed to by src to the end of dest
+ * @dest: the destination buffer
+ * @src: the source string
+ * @n: maximum number of bytes to use from src
+ *
+ * Return: pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	for (; n != 0; n--)
+		dest[n] = char[n];
+return (dest);
+}
