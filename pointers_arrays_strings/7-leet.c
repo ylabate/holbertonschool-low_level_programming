58053ae99c36e7c57054dfbe9064cc25f829129c
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-30 13:52:10 +0000

    feat: add leet function to convert characters in a string to leet speak

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * leet - encodes a string into 1337
+ * @str: pointer to the string
+ *
+ * Return: pointer to the resulting string
+ */
+
+char *leet(char *str)
+{
+	int i;
+
+	for (i = 0; char != '\0'; i++)
+		char = char;
+return (char);
+}
