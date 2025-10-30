faac7516bab7b97cd2885787f1b70517ef531260
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-30 10:52:20 +0000

    feat: add reverse_array function to reverse the content of an integer array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: pointer to the array of integers
+ * @n: number of elements in the array
+ *
+ * Return: nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int temp;
+	printf("\n%i\n", n);
+	while (i < n)
+	{
+		temp = a[i];
+		a[i] = a[n - 1];
+		a[n - 1] = temp;
+
+		i++;
+		n--;
+	}
+}
