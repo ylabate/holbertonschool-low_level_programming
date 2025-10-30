7ed367510879916cd6f57d4f0f8f13506b4e6a45
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-30 13:16:58 +0000

    feat: add cap_string function to capitalize the first letter of each word in a string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: pointer to the string
+ *
+ * Return: pointer to the resulting string
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+			str[i] == ',' || str[i] == '!' || str[i] == '?' ||
+			str[i] == '"' || str[i] == '(' || str[i] == '{' ||
+			str[i] == '}' || str[i] == ';' || str[i] == '.' ||
+			str[i] == ')')
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+				str[i + 1] = (str[i + 1] - 32);
+	}
+return (str);
+}
