0500adf144d39cc675f7a77b7501d797f936eefa
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-30 09:29:45 +0000

    fix: adjust formatting in _strncpy implementation for clarity

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -16,10 +16,11 @@ char *_strncpy(char *dest, char *src, int n)
 	for (i = 0; i <= n && dest[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
-    while (i < n)
-    {
-        dest[i] = '\0';
-        i++;
-    }
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 return (dest);
 }
