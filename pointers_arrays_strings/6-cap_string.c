0d75a50c13be5b18eb66aae1b9d17351cc3174bc
ylabate <236388824+ylabate@users.noreply.github.com>
2025-10-30 13:24:40 +0000

    fix: correct capitalization logic in cap_string function

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -22,8 +22,8 @@ char *cap_string(char *str)
 				str[i + 1] = (str[i + 1] - 32);
 		}
 		else if (i == 0)
-			if (str[i + 1] >= 97 && str[i + 1] <= 122)
-				str[i + 1] = (str[i + 1] - 32);
+			if (str[i] >= 97 && str[i] <= 122)
+				str[i] = (str[i] - 32);
 	}
 return (str);
 }
