a464a4e7296f7e3041f6d235d34c8e86c1569a27
ylabate <236388824+ylabate@users.noreply.github.com>
2025-11-03 12:48:02 +0000

    feat: add _strpbrk function to search for any set of bytes in a string

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: pointer to the string to search
+ * @accept: pointer to the string containing characters to match
+ *
+ * Return: pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, a;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (a = 0; accept[a] != '\0'; a++)
+			if (s[i] == accept[a])
+			{
+				return (&s[i]);
+			}
+	}
+	return (&s[i]);
+}
