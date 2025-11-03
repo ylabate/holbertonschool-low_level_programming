882265da24dc2925f1a84816e8d4ab0c774a2f83
ylabate <236388824+ylabate@users.noreply.github.com>
2025-11-03 09:36:46 +0000

    feat: add _strchr function to locate a character in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to the string to search
+ * @c: character to locate
+ *
+ * Return: pointer to the first occurrence of c in s, or NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == c)
+			return (&s[i]);
+	return ('\0');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -23,5 +23,6 @@ char *leet(char *);
 int _atoi(char *s);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif
