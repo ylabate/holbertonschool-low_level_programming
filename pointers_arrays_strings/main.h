9e17636f6175a7866e5ff81bf34a45f3f013fdd2
ylabate <236388824+ylabate@users.noreply.github.com>
2025-11-03 09:29:35 +0000

    feat: implement _memcpy function to copy memory area

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -22,5 +22,6 @@ char *cap_string(char *);
 char *leet(char *);
 int _atoi(char *s);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
