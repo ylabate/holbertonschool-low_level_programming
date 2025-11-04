04a03e40ba5d113adc85030e6dbeaae263c5044c
ylabate <236388824+ylabate@users.noreply.github.com>
2025-11-04 09:28:23 +0000

    feat: add _puts_recursion function to recursively print a string

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: pointer to the string to print
+ *
+ * Description: Recursively prints each character of the string s using
+ * _putchar, and prints a newline when the terminating null byte is reached.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		_putchar('\0');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
---
+++ recursion/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(int);
+void _puts_recursion(char *s);
+
+#endif
