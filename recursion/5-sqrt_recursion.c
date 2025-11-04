ff853fd37a1431d47783897945605216ad2f010c
ylabate <236388824+ylabate@users.noreply.github.com>
2025-11-04 13:18:45 +0000

    feat: add _sqrt_recursion function to calculate the natural square root of a number

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to find the square root of
+ *
+ * Return: the natural square root, or -1 if n does not have a natural
+ * square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n <= 3)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	return (n / _sqrt_recursion(n % 2));
+}
