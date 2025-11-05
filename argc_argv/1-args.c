37855dc66ea3a879828560692e33b1b157551ee8
ylabate <236388824+ylabate@users.noreply.github.com>
2025-11-05 10:22:59 +0000

    feat: add program to count and print the number of command line arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+	(void)argv;
+}
