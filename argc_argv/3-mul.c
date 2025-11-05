83778afc6841d7cd0040610bbdfa6563dd313b12
ylabate <236388824+ylabate@users.noreply.github.com>
2025-11-05 10:35:47 +0000

    feat: implement program to multiply two command line arguments

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the program name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	result = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", result);
+	return (0);
+}
