5efe1ae2bd42be13a4a610e3fb0dafc7588aed27
ylabate <236388824+ylabate@users.noreply.github.com>
2025-11-05 10:47:43 +0000

    feat: implement program to add two command line arguments

---
+++ argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the program name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, result = 0;
+
+	if (argc < 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		result = result + atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
