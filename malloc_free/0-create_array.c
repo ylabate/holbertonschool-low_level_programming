d6c5225357252e11e4b8700acbde515b2761d9a0
ylabate <12196@holbertonstudents.com>
2025-11-10 09:14:09 +0000

    fix: improve create_array function by adding null check for malloc and updating documentation

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,16 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
-/*
+#include <stdio.h>
+/**
+ * create_array - creates an array of chars and initializes it with a char
+ * @size: size of the array to create
+ * @c: character to initialize the array with
  *
+ * Return: pointer to the array, or NULL if it fails or size is 0
  */
 char *create_array(unsigned int size, char c)
 {
-	char *p;
+	char *p = NULL;
 	unsigned int i;
 
-	p = malloc(sizeof(char) * size);
-	if (p == NULL)
+	if (size == 0)
 		return (NULL);
+	p = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 		p[i] = c;
 	return (p);
