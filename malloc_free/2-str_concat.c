dc071dd9adfe9330cfe5920c524b6f8ebbf60310
ylabate <12196@holbertonstudents.com>
2025-11-10 10:01:40 +0000

    fix: remove null checks for input strings in str_concat function

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,10 +17,6 @@ char *str_concat(char *s1, char *s2)
 	int j;
 
 
-	if (s1 == NULL)
-		return (NULL);
-	if (s2 == NULL)
-		return (NULL);
 	for (i = 0 ; s1[i] != '\0' ; i++)
 	;
 	for (j = 0 ; s2[j] != '\0' ; j++)
