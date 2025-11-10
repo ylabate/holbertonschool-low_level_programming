0a01f52eddcddf104e461ca72eff25c6eb6a0237
ylabate <12196@holbertonstudents.com>
2025-11-10 09:15:04 +0000

    fix: add null check for malloc in create_array function

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -16,6 +16,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	p = malloc(sizeof(char) * size);
+	if (p == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		p[i] = c;
 	return (p);
