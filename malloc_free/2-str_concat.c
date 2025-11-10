4100f07aee15d7c8fc63a5218e0c8018b599ce62
ylabate <12196@holbertonstudents.com>
2025-11-10 10:00:45 +0000

    fix: initialize pointer variable and improve null checks in str_concat function

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,18 +12,22 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *s;
+	char *s = NULL;
 	int i;
 	int j;
 
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
+		return (NULL);
+	if (s2 == NULL)
 		return (NULL);
 	for (i = 0 ; s1[i] != '\0' ; i++)
 	;
 	for (j = 0 ; s2[j] != '\0' ; j++)
 	;
 	s = malloc(sizeof(char) * (i + j) + 1);
+	if (s == NULL)
+		return (NULL);
 	for (i = 0 ; s1[i] != '\0' ; i++)
 		s[i] = s1[i];
 	for (j = 0 ; s2[j] != '\0' ; j++)
