e353e94526389a55bdd2e80403511745e08858bf
ylabate <12196@holbertonstudents.com>
2025-11-12 09:30:04 +0000

    fix: handle null pointer for dog struct in print_dog function

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,7 +12,13 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	printf("Name: %s\n", d->name);
+	if (d->name == NULL)
+		printf("Name = (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d->owner == NULL)
+		printf("Owner = (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
