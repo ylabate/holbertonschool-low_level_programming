e02ed301f723fb0652303b5fef0714182c9e88da
ylabate <12196@holbertonstudents.com>
2025-11-12 10:24:33 +0000

    fix: correct memory allocation in string_nconcat to use proper size calculation

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -15,20 +15,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j;
 	char *s;
-
+/* gere les cas NULL */
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	s = malloc(sizeof(s1) + sizeof(s2));
+/* limite la longueure de j a s2 ou a n si n < s2 */
+	for (j = 0 ; s2[j] != '\0' && j < n; j++)
+	;
+	j = (j * sizeof(int));
+/* alloue la mémoire necessaire et return si malloc rate */
+	s = malloc(sizeof(s1) + j);
 	if (s == NULL)
 		return (NULL);
-
+/* fait une copie de s1 dans s*/
 	for (i = 0 ; s1[i] != '\0' ; i++)
 		s[i] = s1[i];
+/* fait une copie de s2 dans s a la suite de strlen de s1*/
 	for (j = 0 ; s2[j] != '\0' && j < n; j++)
 		s[i + j] = s2[j];
+/* rajoute un "fin de chaine de caractère" */
 	s[i + j] = '\0';
 return (s);
 }
