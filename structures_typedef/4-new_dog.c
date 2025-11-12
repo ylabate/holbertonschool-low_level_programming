6339bf40a893fa37d03dbea718a89eaf560c570c
ylabate <12196@holbertonstudents.com>
2025-11-12 10:01:26 +0000

    fix: improve memory management in new_dog function

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - creates a new dog
@@ -14,10 +15,23 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
+	char *savename;
+	char *saveowner;
 
 	newdog = malloc(sizeof(dog_t));
+	savename = malloc(strlen(name) + 1);
+	saveowner = malloc(strlen(owner) + 1);
+
+	savename = name;
+	saveowner = owner;
+	(void)savename;
+	(void)saveowner;
+
 	if (newdog == NULL)
+	{
+		free(newdog);
 		return (NULL);
+	}
 	if (name != NULL && owner != NULL)
 	{
 		newdog->name = name;
@@ -25,6 +39,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		newdog->owner = owner;
 	}
 	else
+	{
+		free(newdog);
 		return (NULL);
+	}
 	return (newdog);
 }
