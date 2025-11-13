946023bc8f228369daab336804bce9f2bf7266b7
ylabate <12196@holbertonstudents.com>
2025-11-13 10:53:40 +0000

    feat: implement strtow function to split a string into words

---
+++ malloc_free/101-strtow.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - splits a string into words
+ * @str: the string to split
+ *
+ * Return: pointer to an array of strings (words), or NULL on failure
+ * ---------------------------------
+ * h = position actuelle dans le mot
+ * i = position dans str
+ * c = position dans ptr
+ * j = longueur du mot
+ * a = copie de i pour le garder en buffer
+ * ptr **= le pointeur des string résultant
+ */
+
+char **strtow(char *str)
+{
+	int h, i, j, c, a = 0;
+	char **ptr;
+
+	/* gere les NULL */
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
+	/* compte le nombre de mots pour allouer le double pointeur */
+	for (i = 0 ; str[i] != '\0' ; i++)
+		if (str[i] != ' ')
+		{
+			/* compte la taille de chaque mot*/
+			for (; str[i] != ' ' ; i++)
+			;
+			c++;
+		}
+	ptr = malloc(sizeof(char *) * c + 1);
+
+	/* parcours les mots pour copier */
+	c = 0;
+	for (i = 0 ; str[i] != '\0' ; i++)
+		if (str[i] != ' ')
+		{
+			a = i;
+			/* compte la taille de chaque mot*/
+			for (j = 0; str[i] != ' ' ; i++, j++)
+			;
+			/* alloue la mémoire pour chaque mots*/
+			ptr[c] = malloc(sizeof(char) * j);
+			for (h = 0 ; h < j ; h++)
+				ptr[c][h] = str[a + h];
+			c++;
+		}
+
+return (ptr);
+}
