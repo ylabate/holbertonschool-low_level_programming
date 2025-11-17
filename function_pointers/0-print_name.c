6ee302942fa6390ae2f3e5e50e095169ff8359e9
ylabate <12196@holbertonstudents.com>
2025-11-17 08:53:51 +0000

    Refactor code structure for improved readability and maintainability

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointer.h"
+
+/**
+ * print_name - prints a name using a function pointer
+ * @name: the name to print
+ * @f: function pointer to use for printing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
