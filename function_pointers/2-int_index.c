ac253d22791dd568f7410f9b30f11194430c6db8
ylabate <12196@holbertonstudents.com>
2025-11-17 09:43:38 +0000

    fix: enhance input validation in int_index function

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 	int int_index;
 
-	if (size < 0) /* handle the wrong size case */
+	if (size < 0 || array == NULL || cmp == NULL) /* handle the wrong input case*/
 		return (-1);
 
 	for (i = 0 ; i < size ; i++) /* browse the list */
