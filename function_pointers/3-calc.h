ce77a12d4025c680feb111c83f69b45bc1f9158e
ylabate <12196@holbertonstudents.com>
2025-11-17 14:01:37 +0000

    feat: add arithmetic operation functions and update headers

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,11 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stddef.h>
-
-void print_name(char *name, void (*f)(char *));
-int _putchar(int);
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
 
 /* <calc class = op_function> */
 int op_add(int a, int b);
@@ -14,5 +8,19 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 /* </calc> */
+
+int (*get_op_func(char *s))(int, int);
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
 
 #endif
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+ * op_add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: quotient of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - computes modulo of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of a divided by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
