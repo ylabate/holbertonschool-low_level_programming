bb6abfb3ad6cf2f38b1ef98d24871e5307bc5d81
ylabate <12196@holbertonstudents.com>
2025-11-19 09:28:30 +0000

    fix: format for loop in sum_them_all function for consistency

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -13,7 +13,7 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(args, n);
-	for (i = 0 ; i < n ; i++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
 	va_end(args);
 	return (sum);
