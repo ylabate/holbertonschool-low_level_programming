ecbc93939d7eac9e4ffaca330e62e64871d30b49
ylabate <12196@holbertonstudents.com>
2025-11-19 10:01:36 +0000

    fix: refactor print_strings function for clarity and consistency

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -13,14 +13,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
+	char *str;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-/*		if (va_arg(args, int) == NULL) */
-/*			printf("(nil)"); */
-
-		printf("%s", va_arg(args, char *));
+		str = va_arg(args, char *);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
 		if (separator != NULL && i + 1 < n)
 			printf("%s", separator);
 	}
