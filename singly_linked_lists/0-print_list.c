adc5a5919b56a12d9d4dd758a13b4c6b0432b42c
ylabate <12196@holbertonstudents.com>
2025-12-01 09:53:03 +0000

    feat: implement print_list function to display elements of a singly linked list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The number of nodes printed.
+ */
+
+size_t print_list(const list_t *h)
+{
+	int len = 0;
+
+	for (; h != NULL; len++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%d] %s", h->len, h->str);
+		if (h->next != NULL)
+			printf("\n");
+		h = h->next;
+	}
+	printf("\n");
+	return (len);
+}
