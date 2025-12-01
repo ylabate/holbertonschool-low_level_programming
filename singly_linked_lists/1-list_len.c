f52f841377dd4bcefe94af9e9c44bdc2955352c1
ylabate <12196@holbertonstudents.com>
2025-12-01 09:59:49 +0000

    feat: add function documentation for list_len to improve code clarity

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * list_len - Returns the number of elements in a linked list_t list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: Number of elements in the list.
+ */
 size_t list_len(const list_t *h)
 {
 	int len;
