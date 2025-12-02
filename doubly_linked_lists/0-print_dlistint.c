d26eb7c985060c491489d1e4f574f6eb88629c3f
ylabate <12196@holbertonstudents.com>
2025-12-02 08:51:32 +0000

    fix: ensure print_dlistint handles NULL head pointer gracefully

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -11,9 +11,10 @@ size_t print_dlistint(const dlistint_t *h)
 	const dlistint_t *temp = h;
 	int count = 0;
 
+	if (h == NULL)
+		return (0);
 	while (temp->prev)
 		temp = temp->prev;
-
 	while (temp)
 	{
 		printf("%d\n", temp->n);
