38d698f5277e985bb4191243df4ca343d688fd15
ylabate <12196@holbertonstudents.com>
2025-12-03 14:21:06 +0000

    fix: simplify text appending logic in append_text_to_file function

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -9,7 +9,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, length;
+	int fd;
 
 	if (filename == NULL)
 		return (-1);
@@ -19,8 +19,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	for (length = 0 ; text_content[length] != '\0' ; length++)
-	;
-	write(fd, text_content, length);
+	dprintf(fd, "%s", text_content);
 	return (1);
 }
