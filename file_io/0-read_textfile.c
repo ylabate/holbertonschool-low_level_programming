3c368fa40048bc634ca710721e1bbd422981a049
ylabate <12196@holbertonstudents.com>
2025-12-03 14:14:26 +0000

    fix: improve error handling and buffer management in read_textfile function

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,20 +9,33 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, printed_char = 0;
+	int fd, printed_char = 0, checkexit;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
 	fd = open(filename, O_RDONLY);
-
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 	printed_char = read(fd, buffer, letters);
 	close(fd);
+	if (printed_char == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
-	write(STDOUT_FILENO, buffer, printed_char);
-
+	checkexit = write(STDOUT_FILENO, buffer, printed_char);
 	free(buffer);
+	if (checkexit == -1 || checkexit != printed_char)
+		return (0);
+
 	return (printed_char);
 }
